fdfabac8f707ee44950cc760d45f0ff422113eab
Serge Bakharev <serge.bakharev@gmail.com>
2023-06-17 12:40:19 +1000

    merge util.py into main for portability

--- main.py
+++ main.py
@@ -213,6 +213,17 @@ def write_file(filename, filecode, directory):
         # Write content to the file
         file.write(filecode)
 
+def clean_dir(directory):
+    # Check if the directory exists
+    if os.path.exists(directory):
+        # If it does, iterate over all files and directories
+        for dirpath, _, filenames in os.walk(directory):
+            for filename in filenames:
+                _, extension = os.path.splitext(filename)
+                if extension not in EXTENSION_TO_SKIP:
+                    os.remove(os.path.join(dirpath, filename))
+    else:
+        os.makedirs(directory, exist_ok=True)
 
 if __name__ == "__main__":
 
